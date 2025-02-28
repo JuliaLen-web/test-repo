af34a8e783a1e8186244002407d87680a59cb47c
JuliaLen-web <j.ibatullina@redneckstudio.com>
2025-02-28 09:30:55 +0400

    test

--- javascripts/discourse/app/components/scrolling-post-stream.js
+++ javascripts/discourse/app/components/scrolling-post-stream.js
@@ -43,6 +43,7 @@ export default class ScrollingPostStream extends MountWidget {
 
   buildArgs() {
     return this.getProperties(
+      "mainPost",
       "posts",
       "canCreatePost",
       "filteredPostsCount",
@@ -165,6 +166,7 @@ export default class ScrollingPostStream extends MountWidget {
     const refresh = (cb) => this.queueRerender(cb);
     if (onscreen.length) {
       console.log(posts);
+      console.log(mainPost);
       const first = posts.objectAt(onscreen[0]);
       if (this._topVisible !== first) {
         this._topVisible = first;
